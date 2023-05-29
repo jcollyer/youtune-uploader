813d0d684f9948847cc8f0ba8e15072248551418
jeremy collyer <collyerdesign@gmail.com>
2023-05-29 10:57:49 -0400

    video upload to youtube via node

--- client/components/organisms/Upload/Upload.js
+++ client/components/organisms/Upload/Upload.js
@@ -1,5 +1,134 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { push } from 'redux-first-history';
+import * as R from 'ramda';
+import { Typography, Button, Form, Input } from 'antd';
+import { PlusSquareOutlined } from '@ant-design/icons';
+import Dropzone from 'react-dropzone';
+
+const { Title } = Typography;
+const { TextArea } = Input;
+
+const Private = [
+  { value: 0, label: 'Private' },
+  { value: 1, label: 'Public' },
+];
+
+const Catogory = [
+  { value: 0, label: 'Film & Animation' },
+  { value: 0, label: 'Autos & Vehicles' },
+  { value: 0, label: 'Music' },
+  { value: 0, label: 'Pets & Animals' },
+  { value: 0, label: 'Sports' },
+];
 
 export default function Upload() {
-  return <div>Upload here.</div>;
+  const dispatch = useDispatch();
+  const { user } = useSelector(R.pick(['user']));
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (R.isEmpty(user)) {
+      dispatch(push('/login'));
+    } else {
+      setLoading(false);
+    }
+  }, [dispatch, user]);
+
+  const [title, setTitle] = useState('');
+  const [Description, setDescription] = useState('');
+  const [privacy, setPrivacy] = useState(0);
+  const [Categories, setCategories] = useState('Film & Animation');
+
+  const handleChangeTitle = event => {
+    setTitle(event.currentTarget.value);
+  };
+
+  const handleChangeDecsription = event => {
+    console.log(event.currentTarget.value);
+
+    setDescription(event.currentTarget.value);
+  };
+
+  const handleChangeOne = event => {
+    setPrivacy(event.currentTarget.value);
+  };
+
+  const handleChangeTwo = event => {
+    setCategories(event.currentTarget.value);
+  };
+
+  const onSubmit = () => {};
+
+  return (
+    <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+      <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+        <Title level={2}> Upload Video</Title>
+      </div>
+
+      <Form onSubmit={onSubmit}>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Dropzone multiple={false} maxSize={800000000}>
+            {({ getRootProps, getInputProps }) => (
+              <div
+                style={{
+                  width: '300px',
+                  height: '240px',
+                  border: '1px solid lightgray',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+                {...getRootProps()}>
+                <input {...getInputProps()} />
+                <PlusSquareOutlined style={{ fontSize: '3rem' }} />
+              </div>
+            )}
+          </Dropzone>
+
+          {/* {thumbnail !== '' &&
+                <div>
+                    <img src={`http://localhost:5000/${thumbnail}`} alt='haha' />
+                </div>
+            } */}
+        </div>
+
+        <br />
+        <br />
+        <label>Title</label>
+        <Input onChange={handleChangeTitle} value={title} />
+        <br />
+        <br />
+        <label>Description</label>
+        <TextArea onChange={handleChangeDecsription} value={Description} />
+        <br />
+        <br />
+
+        <select onChange={handleChangeOne}>
+          {Private.map((item, index) => (
+            <option key={index} value={item.value}>
+              {item.label}
+            </option>
+          ))}
+        </select>
+        <br />
+        <br />
+
+        <select onChange={handleChangeTwo}>
+          {Catogory.map((item, index) => (
+            <option key={index} value={item.label}>
+              {item.label}
+            </option>
+          ))}
+        </select>
+        <br />
+        <br />
+
+        <Button type="primary" size="large" onClick={onSubmit}>
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 }
