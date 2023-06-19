5596694f81bd71ff65dc076c17369eb41cd852ad
jeremy collyer <collyerdesign@gmail.com>
2023-06-19 07:32:38 -0400

    update mongoose to v7 and update two routes to test on vercel

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,21 +1,18 @@
 const express = require('express');
 const { User } = require('../../server/database/schemas');
-
 const router   = express.Router();
-
 module.exports = router;
 
-router.post('/checkusername', (req, res) => {
+router.post('/checkusername', async (req, res) => {
   const username = req.body.username.toLowerCase();
-
-  User.find({ username }, (err, users) => {
-    if (err) {
-      res.status(400).send({ message: 'Check username failed', err, username });
-    }
-    if (users && users[0]) {
+  try {
+    const data = User.find({ username });
+    if (data && data[0]) {
       res.send({ available: false, message: 'Username exists', username });
     } else {
       res.send({ available: true, message: 'Username available', username });
     }
-  });
+  } catch (error) { 
+    res.status(400).send({ message: 'Check username failed', err, username });
+  }
 });
