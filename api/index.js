82ba449fbb16310715a373a755279bb2f63f3197
jeremy collyer <collyerdesign@gmail.com>
2023-07-24 18:18:25 -0400

    referer

--- api/index.js
+++ api/index.js
@@ -40,7 +40,7 @@ app.use(cors(corsOptions));
 const allowlist = ['https://youtune-uploader.vercel.app', 'http://localhost:3000'];
 
 app.use((req, res, next) => {
-  console.log('------------------>>', req.header('Origin'))
+  console.log('---------- header origin -------->>', req.header('origin'), req.headers.referer)
   if (allowlist.indexOf(req.header('Origin')) !== -1) {
     res.setHeader('Access-Control-Allow-Origin', req.header('Origin'));
   }
