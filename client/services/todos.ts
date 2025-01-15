53f3b06e03c53b66e454cab8b5383254637c80ef
jeremy collyer <collyerdesign@gmail.com>
2025-01-15 11:59:13 -0800

    type update on todos

--- client/services/todos.ts
+++ client/services/todos.ts
@@ -1,7 +1,7 @@
 import request from 'superagent';
 import { handleSuccess, handleError } from '../utils/api';
 
-export const postTodo = (info:any) =>
+export const postTodo = (info:{text: string}) =>
   request.post('/api/todos')
     .send(info)
     .then(handleSuccess)
@@ -12,19 +12,19 @@ export const getTodos = () =>
     .then(handleSuccess)
     .catch(handleError);
 
-export const putToggleCompleteTodo = (info:any) =>
+export const putToggleCompleteTodo = (info:{text: string}) =>
   request.put('/api/todos/complete')
     .send(info)
     .then(handleSuccess)
     .catch(handleError);
 
-export const putTodo = (info:any) =>
+export const putTodo = (info:{text: string}) =>
   request.put('/api/todos')
     .send(info)
     .then(handleSuccess)
     .catch(handleError);
 
-export const deleteTodo = (info:any) =>
+export const deleteTodo = (info:{text: string}) =>
   request.delete('/api/todos')
     .send(info)
     .then(handleSuccess)
